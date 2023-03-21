7939962c4141ca60c6da5d354c2a572adb54aa70
Andrew <andrewvt97@gmail.com>
2023-03-21 00:00:21 -0400

    Add function to remove subarray from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(listOfNum, ...args) {
 
+   
+   
+    for (let i = listOfNum.length -1 ; i >= 0; i--){
+        console.log(listOfNum[i]);
+        if (args.includes(listOfNum[i])){
+            console.log(true);
+            listOfNum.splice(i, 1);
+        }
+    }
+
+    return listOfNum;
+    
 };
+
+removeFromArray([1, 2, 3, 4], 3, 2);
 
 // Do not edit below this line
 module.exports = removeFromArray;
