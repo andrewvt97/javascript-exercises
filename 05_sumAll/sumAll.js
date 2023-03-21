1a914d510694fae4c4398768480e3b366407da49
Andrew <andrewvt97@gmail.com>
2023-03-21 00:21:44 -0400

    Add function to add numbers in range

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,30 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
+    if (num1 < 0 || num2 < 0){
+        return("ERROR");
+    }
+
+    if (typeof num1 !== 'number' || typeof num2 !== 'number'){
+        return('ERROR');
+    }
+
+    let start = num1, end = num2, sum = 0;
+
+    if (num1 > num2){
+        start = num2;
+        end = num1;
+    }
+    
+
+    for (let i = start; i <= end; i++){
+        sum += i;
+        console.log(sum);
+    }
+
+    
+    return sum;
 };
 
+console.log(sumAll(1,4));
 // Do not edit below this line
 module.exports = sumAll;
