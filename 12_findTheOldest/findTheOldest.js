fd2a4948c3a795918023640bc51a82b4dae07f4b
Andrew <andrewvt97@gmail.com>
2023-04-01 17:45:50 -0400

    Add function to find oldest person

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,48 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    const sortedAge = array.sort((a,b) => {
+   
+        if (a.yearOfDeath === undefined){
+            deathA = (new Date()).getFullYear();
+        }
+        else{
+            deathA = a.yearOfDeath
+        }
+        if (b.yearOfDeath === undefined){
+            deathB = (new Date()).getFullYear();
+            console.log(deathB);
+        }
+        else{
+            deathB = b.yearOfDeath
+        }
+        const lastPerson = deathA - a.yearOfBirth;
+        const nextPerson = deathB - b.yearOfBirth;
 
+        if (lastPerson > nextPerson){
+            return -1;
+        }
+        else{
+            return 1;
+        }
+    });
+    return sortedAge[0];
 };
 
+//   const people = [
+//     {
+//       name: "Carly",
+//       yearOfBirth: 1066,
+//     },
+//     {
+//       name: "Ray",
+//       yearOfBirth: 1962,
+//       yearOfDeath: 2011,
+//     },
+//     {
+//       name: "Jane",
+//       yearOfBirth: 1912,
+//       yearOfDeath: 1941,
+//     },
+//   ]
+// console.table(findTheOldest(people));
 // Do not edit below this line
 module.exports = findTheOldest;
