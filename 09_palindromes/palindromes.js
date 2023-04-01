88dc5235a701e223e97619394156953ad64f3721
Andrew <andrewvt97@gmail.com>
2023-04-01 16:14:08 -0400

    Add palindrome function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,29 @@
-const palindromes = function () {
-
+const palindromes = function (phrase) {
+    let i = 0, j = phrase.length -1;
+    outer: while (i < j){
+        while ("!?.,'\":; ".includes(phrase[i])){
+            i++;
+            if (i >= j){
+                continue outer;
+            }
+        }
+        while ("!?.,'\":; ".includes(phrase[j])){ // includes is javascript method to check if string includes substring
+            j--;
+            if (i >= j){
+                continue outer;
+            }
+        }
+        if (phrase[i].toLowerCase() != phrase[j].toLowerCase()){
+            return false
+    }
+    i++;
+    j--;
+            
+    }
+    return true;
 };
+
+console.log(palindromes('ZZZZ car, a man, a maracaz.'));
 
 // Do not edit below this line
 module.exports = palindromes;
