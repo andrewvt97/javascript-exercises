f98f8b4f3ae176767022c7a8787accc415d14ba7
Andrew <andrewvt97@gmail.com>
2023-04-01 16:22:29 -0400

    Add fibonacci function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 1){
+        return "OOPS";
+    }
+    if (num == 1 || num == 2){
+        return 1;
+    }
+    return fibonacci(num-1) + fibonacci(num-2)
 };
 
+// console.log(fibonacci(-3));
 // Do not edit below this line
 module.exports = fibonacci;
